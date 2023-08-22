99c94edb6be55706cc0fbccf7961f320bad8e22f
Dmitry Sorokin <dmd40in@gmail.com>
2023-08-22 11:40:57 +0100

    add test 3
    Signed-off-by: Dmitry Sorokin <dmd40in@gmail.com>

--- test_example.py
+++ test_example.py
@@ -3,7 +3,7 @@ from example import hello_world
 
 class TestHello(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(hello_world(), "Hello, GitHub Actions! 2")
+        self.assertEqual(hello_world(), "Hello, GitHub Actions!")
 
 if __name__ == "__main__":
     unittest.main()
